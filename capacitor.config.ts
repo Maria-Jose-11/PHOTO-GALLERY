20fcc47116bfa747b0d3eaacce76db204c8ce41e
Majo Chalá <74751902+Maria-Jose-11@users.noreply.github.com>
2022-11-16 23:42:27 -0500

    Galeria de fotos funcional

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'photo-gallery-with-capacitor',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
